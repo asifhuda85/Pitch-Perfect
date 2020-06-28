965c95ee6cadbabe210a620cda896b68de744c58
Md Asif Huda <asiflaw@gmail.com>
2020-06-27 22:08:08 -0400

    Setup Project for Pitch Perfect

---
+++ Pitch Perfect/Pitch Perfect/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pitch Perfect
+//
+//  Created by Md Asif Huda on 6/27/20.
+//  Copyright © 2020 Md Asif Huda. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
